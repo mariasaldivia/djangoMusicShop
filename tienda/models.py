9fccd535ec5735435b53404c3240327bc0f8d5f5
mariasaldivia <mari.saldivia@duocuc.cl>
2023-07-06 23:56:51 -0400

    Se agrego atributo de destacado a Artista y se muestra en la interfaz gráfica

--- tienda/models.py
+++ tienda/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Artista(models.Model):
     nombre = models.CharField(max_length=70)
     imagen = models.ImageField(upload_to="artistas",null=True)
+    destacado = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nombre
